cb7cb8f3463d6e315bf089ae4f070b145d5089a7
Dmitri Proskuriakov <dmitri.proskuriakov@gmail.com>
2022-11-24 00:14:51 +0300

    custom noise in world gen

--- src/World/World.cpp
+++ src/World/World.cpp
@@ -1,65 +1,82 @@
 // Copyright 2021-2022 JustDprroz
 
 #include "World/World.hpp"
+#include <nlohmann/json.hpp>
 
-World::World() {
+extern nlohmann::json data;
 
+World::World() : noise_gen_(0)
+{
 }
 
-World::World(int seed) : seed_(seed) {
-
+World::World(int seed) : seed_(seed), noise_gen_(seed)
+{
 }
 
-void World::SetSeed(int seed) {
+void World::SetSeed(int seed)
+{
     seed_ = seed;
 }
 
-std::vector<Entity*>& World::GetEntities() {
+std::vector<Entity *> &World::GetEntities()
+{
     return entities_;
 }
 
 // TODO: add entity management system
-void World::SummonEntity(Entity &entity){
+void World::SummonEntity(Entity &entity)
+{
     entities_.push_back(&entity);
 }
 
-Block World::GetBlock(sf::Vector2i position) {
-    sf::Vector2i chunk_position = {div<int>(position.x, kChunkSize),div<int>(position.y, kChunkSize)};
-    if (!IsChunkExist(chunk_position)) return {INT_MIN};
-    return chunks_[{chunk_position.x, chunk_position.y}].GetBlock({mod<int>(position.x, kChunkSize),mod<int>(position.y, kChunkSize)});
+Block World::GetBlock(sf::Vector2i position)
+{
+    sf::Vector2i chunk_position = {div<int>(position.x, kChunkSize), div<int>(position.y, kChunkSize)};
+    if (!IsChunkExist(chunk_position))
+        return {INT_MIN};
+    return chunks_[{chunk_position.x, chunk_position.y}].GetBlock({mod<int>(position.x, kChunkSize), mod<int>(position.y, kChunkSize)});
 }
 
-void World::PlaceBlock(sf::Vector2i position, Block &block) {
-    chunks_[{div<int>(position.x, kChunkSize),div<int>(position.y, kChunkSize)}].PlaceBlock({mod<int>(position.x, kChunkSize),mod<int>(position.y, kChunkSize)}, block);
+void World::PlaceBlock(sf::Vector2i position, Block &block)
+{
+    chunks_[{div<int>(position.x, kChunkSize), div<int>(position.y, kChunkSize)}].PlaceBlock({mod<int>(position.x, kChunkSize), mod<int>(position.y, kChunkSize)}, block);
 }
 
-bool World::IsChunkExist(sf::Vector2i position) {
+bool World::IsChunkExist(sf::Vector2i position)
+{
     return chunks_.find({position.x, position.y}) != chunks_.end();
 }
 
-Chunk* World::GetChunk(sf::Vector2i position) {
-    if (!IsChunkExist({position.x, position.y})) {
+Chunk *World::GetChunk(sf::Vector2i position)
+{
+    if (!IsChunkExist({position.x, position.y}))
+    {
         PlaceChunk(position);
-        GenerateChunk(position);
+        GenerateChunkWIP(position);
     }
     return &chunks_[{position.x, position.y}];
 }
 
-void World::PlaceChunk(sf::Vector2i position) {
-    if (IsChunkExist(position)) return;
+void World::PlaceChunk(sf::Vector2i position)
+{
+    if (IsChunkExist(position))
+        return;
     chunks_[{position.x, position.y}] = Chunk(position);
 }
 
-// FIXME: normal serialization
-void World::SaveChunks(std::string file_path) {
+// TODO: normal serialization
+void World::SaveChunks(std::string file_path)
+{
     std::ofstream chunkslist;
     chunkslist.open(file_path + "/chunkslist.tem");
-    if (!chunkslist.is_open()) {
+    if (!chunkslist.is_open())
+    {
         std::cout << "Failed open " << file_path + "/chunkslist.tem" << '\n';
         return;
     }
     int lcx, lcy;
-    for (std::map<std::pair<int, int>, Chunk>::iterator it = chunks_.begin(); it != chunks_.end(); ++it) {
+    for (std::map<std::pair<int, int>, Chunk>::iterator it = chunks_.begin(); it != chunks_.end(); ++it)
+    {
         lcx = it->first.first;
         lcy = it->first.second;
         chunkslist.write(reinterpret_cast<char *>(&lcx), sizeof(lcx));
@@ -69,16 +86,19 @@ void World::SaveChunks(std::string file_path) {
     chunkslist.close();
 }
 
-// FIXME: normal deserialization
-void World::LoadChunks(std::string file_path) {
+// TODO: normal deserialization
+void World::LoadChunks(std::string file_path)
+{
     std::ifstream chunkslist;
     chunkslist.open(file_path + "/chunkslist.tem");
-    if (!chunkslist.is_open()) {
+    if (!chunkslist.is_open())
+    {
         std::cout << "Failed open " << file_path + "/chunkslist.tem" << '\n';
         return;
     }
     int lcx, lcy;
-    while (!chunkslist.eof()) {
+    while (!chunkslist.eof())
+    {
         chunkslist.read(reinterpret_cast<char *>(&lcx), sizeof(lcx));
         chunkslist.read(reinterpret_cast<char *>(&lcy), sizeof(lcy));
         chunks_[{lcx, lcy}] = Chunk({lcx, lcy});
@@ -88,28 +108,76 @@ void World::LoadChunks(std::string file_path) {
 }
 
 // TODO: Great world generation
-void World::GenerateChunk(sf::Vector2i position) {
-    for (int i = 0; i < kChunkSize; i++) {
-        for (int j = 0; j < kChunkSize; j++) {
-            float noiseHeightValue = noise_block_.GetNoise(static_cast<float>(i + position.x * kChunkSize), static_cast<float>(j + position.y * kChunkSize));
+// void World::GenerateChunk(sf::Vector2i position)
+// {
+//     for (int i = 0; i < kChunkSize; i++)
+//     {
+//         for (int j = 0; j < kChunkSize; j++)
+//         {
+//             float noiseHeightValue = noise_block_.GetNoise(static_cast<float>(i + position.x * kChunkSize), static_cast<float>(j + position.y * kChunkSize));
+//             int blockId = static_cast<int>(LinearInterpolation(noiseHeightValue, -1.f, 1.0f, 0.0f, 2.0f));
+
+//             float noiseBiomeValue = noise_biome_.GetNoise(static_cast<float>(i + position.x * kChunkSize), static_cast<float>(j + position.y * kChunkSize));
+//             float biomeInterpolated = LinearInterpolation(noiseBiomeValue, -1.f, 1.0f, 0.0f, 3.0f);
+
+//             int biomeId = 0;
+//             if (InRange(biomeInterpolated, 0.0f, 0.5f))
+//             {
+//                 biomeId = 0;
+//             }
+//             else if (InRange(biomeInterpolated, 0.5f, 0.9f))
+//             {
+//                 biomeId = 1;
+//             }
+//             else if (InRange(biomeInterpolated, 0.9f, 1.1f))
+//             {
+//                 biomeId = 2;
+//             }
+//             chunks_[{position.x, position.y}].PlaceBlock({i, j}, {blockId, 0});
+//         }
+//     }
+//     chunks_[{position.x, position.y}].SetGenerated();
+// }
+
+void World::GenerateChunkWIP(sf::Vector2i position)
+{
+    for (int i = 0; i < kChunkSize; i++)
+    {
+        for (int j = 0; j < kChunkSize; j++)
+        {
+            float noiseHeightValue = noise_gen_.EvaluateFBM(
+                static_cast<float>(i + position.x * kChunkSize + 1'000'000),
+                static_cast<float>(j + position.y * kChunkSize + 1'000'000),
+                data["AMPLITUDE"],
+                data["FREQUENCY"],
+                data["OCTAVES"]
+            );
             int blockId = static_cast<int>(LinearInterpolation(noiseHeightValue, -1.f, 1.0f, 0.0f, 2.0f));
-            float noiseBiomeValue = noise_biome_.GetNoise(static_cast<float>(i + position.x * kChunkSize), static_cast<float>(j + position.y * kChunkSize));
+
+            float noiseBiomeValue = noise_gen_.noise(static_cast<float>(i + position.x * kChunkSize), static_cast<float>(j + position.y * kChunkSize), 1000.0);
             float biomeInterpolated = LinearInterpolation(noiseBiomeValue, -1.f, 1.0f, 0.0f, 3.0f);
+
             int biomeId = 0;
-            if (InRange(biomeInterpolated, 0.0f, 0.5f)) {
+            if (InRange(biomeInterpolated, 0.0f, 0.5f))
+            {
                 biomeId = 0;
-            } else if (InRange(biomeInterpolated, 0.5f, 0.9f)) {
+            }
+            else if (InRange(biomeInterpolated, 0.5f, 0.9f))
+            {
                 biomeId = 1;
-            } else if (InRange(biomeInterpolated, 0.9f, 1.1f)) {
+            }
+            else if (InRange(biomeInterpolated, 0.9f, 1.1f))
+            {
                 biomeId = 2;
             }
-            chunks_[{position.x, position.y}].PlaceBlock({i, j}, {blockId, biomeId});
+            chunks_[{position.x, position.y}].PlaceBlock({i, j}, {blockId, 0});
         }
     }
     chunks_[{position.x, position.y}].SetGenerated();
 }
 
-void World::NoiseInit() {
+void World::NoiseInit()
+{
     noise_block_.SetSeed(seed_);
     noise_block_.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2S);
     noise_block_.SetFrequency(0.02);
--- src/World/World.hpp
+++ src/World/World.hpp
@@ -1,7 +1,7 @@
 // Copyright 2021-2022 JustDprroz
 
-#ifndef SOURCE_WORLD_WORLD_HPP_
-#define SOURCE_WORLD_WORLD_HPP_
+#ifndef SEQUOIA_ENGINE_SRC_WORLD_WORLD_
+#define SEQUOIA_ENGINE_SRC_WORLD_WORLD_
 
 #include <cstdlib>
 #include <ctime>
@@ -18,6 +18,7 @@
 #include <Utils.hpp>
 #include <climits>
 #include <SFML/Window/Event.hpp>
+#include <external/noise.hpp>
 
 #include <World/Chunk.hpp>
 #include <World/Block.hpp>
@@ -39,15 +40,17 @@ public:
     void PlaceChunk(sf::Vector2i position);
     void LoadChunks(std::string file_path);
     void SaveChunks(std::string file_path);
-    void GenerateChunk(sf::Vector2i position);
+    // void GenerateChunk(sf::Vector2i position);
+    void GenerateChunkWIP(sf::Vector2i position);
     void NoiseInit();
 private:
     std::map<std::pair<int, int>, Chunk> chunks_;
     std::vector<Entity*> entities_;
     FastNoiseLite noise_block_;
     FastNoiseLite noise_biome_;
+    PerlinNoise noise_gen_;
     int seed_;
     friend class RenderWorld;
 };
 
-#endif  // SOURCE_WORLD_WORLD_HPP_
+#endif /* SEQUOIA_ENGINE_SRC_WORLD_WORLD_ */
